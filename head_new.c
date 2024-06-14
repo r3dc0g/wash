87420b52f136dd2de44232e7bf68c0a4ff2ec7a7
r3dc0g <critesg@spu.edu>
2024-06-13 18:38:32 -0700

    Finished redirection

---
+++ head_new.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    int current_opt;
+    int line_numbers = 0;
+
+    while ((current_opt = getopt(argc, argv, "hn:")) != -1) {
+        switch (current_opt) {
+            case 'h':
+                printf("Usage: %s <filename>\n", argv[0]);
+                return 0;
+            case 'n':
+                line_numbers = strtol(optarg);
+                break;
+            default:
+                printf("Usage: %s <filename>\n", argv[0]);
+                return 1;
+        }
+    }
+
+    FILE *file = fopen(argv[argc - 1], "r");
+    if (file == NULL) {
+        printf("Error: Cannot open file %s\n", argv[argc - 1]);
+        return 1;
+    }
+
+    int i = 0;
+    while (!feof(file)) {
+        char *line;
+        size_t length = 0;
+        getline(&line, &length, file);
+        fprintf(stdout, "%s", line);
+    }
+
+    return 0;
+}
+
